b33796f68109162cf4e212c6de2f364da817f2d2
Cadavid06 <carlos.ricardoreiino@gmail.com>
2025-10-14 11:09:03 -0500

    cron

--- backend/src/cron/membership.cron.js
+++ backend/src/cron/membership.cron.js
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import MemberShip from "../models/memberShip.models.js";
 
 // Se ejecuta a las 00:00 del día 1 de cada mes
-cron.schedule("* * * * *", async () => {
+cron.schedule("0 0 1 * *", async () => {
   console.log("Ejecutando cron job para actualizar membresías...");
 
   // Actualizar todas las membresías al estado "Expirada"
